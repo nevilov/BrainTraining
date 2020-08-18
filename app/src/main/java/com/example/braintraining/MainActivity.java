665abafcf1ac524a00fd8092a735a508440f7a6a
Alim Nevilov <52937912+nevilov@users.noreply.github.com>
2020-08-18 20:11:01 +0300

    V-0.1 (SharedPreferences)

--- app/src/main/java/com/example/braintraining/MainActivity.java
+++ app/src/main/java/com/example/braintraining/MainActivity.java
@@ -5,9 +5,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
     private SharedPreferences preferences;
+    private final String Key = "Save_key";
+    private EditText editText;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,14 +23,19 @@ public class MainActivity extends AppCompatActivity {
 
 
     public void save(View view) {
+        SharedPreferences.Editor edit = preferences.edit();
+        edit.putString(Key, editText.getText().toString()); // put storage
+        edit.apply(); // save storage
     }
 
     public void get(View view){
+        editText.setText(preferences.getString(Key,"Empty")); // get storage
 
     }
 
     private void init(){
         preferences = getSharedPreferences("MainPreferences", MODE_PRIVATE);
-
+        editText = findViewById(R.id.editText);
+        editText.setText(preferences.getString(Key,"Empty"));
     }
 }
